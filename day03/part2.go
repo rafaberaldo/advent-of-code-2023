df57226dff4be2b516517970ab770e831f2f7acb
Rafael Beraldo <rafael.pimpa@gmail.com>
2024-07-12 20:52:42 -0300

    d3p2

---
+++ day03/part2.go
@@ -0,0 +1,86 @@
+package day03
+
+import (
+	"regexp"
+	"strconv"
+)
+
+type PartCoord struct {
+	row       int
+	spaces    []int
+	value     string
+	neighbors []string
+}
+
+func Part2() int {
+	var input = GetInput()
+	var count = 0
+	var partsCoord = []PartCoord{}
+	var reNum = regexp.MustCompile(`\d+|[^\.]`)
+
+	var intRange = func(min int, max int) []int {
+		var result = []int{}
+		for i := min; i <= max; i++ {
+			result = append(result, i)
+		}
+		return result
+	}
+
+	for row := 0; row < len(input); row++ {
+		var iMatches = reNum.FindAllStringIndex(input[row], -1)
+		var matches = reNum.FindAllString(input[row], -1)
+
+		for i := 0; i < len(iMatches); i++ {
+			var part = PartCoord{row, intRange(iMatches[i][0], iMatches[i][1]), matches[i], nil}
+			partsCoord = append(partsCoord, part)
+		}
+	}
+
+	var comparePartsCoord = func(a PartCoord, b PartCoord) bool {
+		return a.row == b.row && a.value == b.value && a.spaces[0] == b.spaces[0]
+	}
+
+	var findNeighbors = func(part *PartCoord) {
+		for _, p := range partsCoord {
+			if part.row-p.row > 1 || part.row-p.row < -1 || comparePartsCoord(*part, p) {
+				continue
+			}
+
+		outer:
+			for i := 0; i < len(part.spaces); i++ {
+				for j := 0; j < len(p.spaces); j++ {
+					if part.spaces[i] == p.spaces[j] {
+						part.neighbors = append(part.neighbors, p.value)
+						break outer
+					}
+				}
+			}
+		}
+	}
+
+	for i := 0; i < len(partsCoord); i++ {
+		findNeighbors(&partsCoord[i])
+
+		// fmt.Printf("%+v\n", partsCoord[i])
+
+		if len(partsCoord[i].neighbors) == 0 {
+			continue
+		}
+
+		if partsCoord[i].value != "*" {
+			continue
+		}
+
+		if len(partsCoord[i].neighbors) != 2 {
+			continue
+		}
+
+		if value1, err := strconv.Atoi(partsCoord[i].neighbors[0]); err == nil {
+			if value2, err := strconv.Atoi(partsCoord[i].neighbors[1]); err == nil {
+				count += value1 * value2
+			}
+		}
+	}
+
+	return count
+}
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/rafaberaldo/advent-of-code/day02"
+	"github.com/rafaberaldo/advent-of-code/day03"
 )
 
 func main() {
 	// fmt.Println("Day01-Part1:", day01.Part1())
 	// fmt.Println("Day01-Part1:", day01.Part2())
 	// fmt.Println("Day02-Part1:", day02.Part1())
-	fmt.Println("Day02-Part2:", day02.Part2())
+	// fmt.Println("Day02-Part2:", day02.Part2())
+	// fmt.Println("Day02-Part2:", day03.Part1())
+	fmt.Println("Day02-Part2:", day03.Part2())
 }
