0e2a5b1e130c9bab2b430ae037055250630ab297
Rafael Beraldo <rafael.pimpa@gmail.com>
2024-07-14 16:23:33 -0300

    d5p1

--- main.go
+++ main.go
@@ -3,16 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/rafaberaldo/advent-of-code/day04"
+	"github.com/rafaberaldo/advent-of-code/day05"
 )
 
 func main() {
-	// fmt.Println("Day01-Part1:", day01.Part1())
-	// fmt.Println("Day01-Part1:", day01.Part2())
-	// fmt.Println("Day02-Part1:", day02.Part1())
-	// fmt.Println("Day02-Part2:", day02.Part2())
-	// fmt.Println("Day02-Part2:", day03.Part1())
-	// fmt.Println("Day02-Part2:", day03.Part2())
-	// fmt.Println("Day02-Part2:", day04.Part1())
-	fmt.Println("Day02-Part2:", day04.Part2())
+	fmt.Println("Result:", day05.Part1())
 }
