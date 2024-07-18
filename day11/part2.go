e4c5a1b0b43090dfe8025f28e292d8edbba7c9e0
Rafael Beraldo <rafael.pimpa@gmail.com>
2024-07-18 20:22:50 -0300

    add concurrency

--- day11/part2.go
+++ day11/part2.go
@@ -14,23 +14,18 @@ func Part2() int {
 		log.Fatal(err)
 	}
 
-	var result1 int
+	var c = make(chan int)
+	defer close(c)
+
 	var expand1 = 1
-	var points = parseInput(input, expand1)
-	var pairs = getPairs(points)
-	for _, pair := range pairs {
-		result1 += getDistance(pair.a, pair.b)
-	}
+	go getResult(input, expand1, c)
+	var result1 = <-c
 
-	var result2 int
-	var espand2 = 2
-	points = parseInput(input, espand2)
-	pairs = getPairs(points)
-	for _, pair := range pairs {
-		result2 += getDistance(pair.a, pair.b)
-	}
+	var expand2 = 2
+	go getResult(input, expand2, c)
+	var result2 = <-c
 
-	var slope = (result2 - result1) / (espand2 - expand1)
+	var slope = (result2 - result1) / (expand2 - expand1)
 	var diff = result1 - slope
 	var result = slope*1_000_000 + diff
 
@@ -38,4 +33,14 @@ func Part2() int {
 	fmt.Printf("Done in %s\n", elapsed)
 
 	return result
+}
+
+func getResult(input []byte, expand int, c chan int) {
+	var result int
+	var points = parseInput(input, expand)
+	var pairs = getPairs(points)
+	for _, pair := range pairs {
+		result += getDistance(pair.a, pair.b)
+	}
+	c <- result
 }
